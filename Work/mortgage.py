294dd986d496e65ad0543016a7099d2e1e211fb7
elin-croft <wangyilin19941@outlook.com>
2020-06-02 19:57:11 +0800

    finish mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,20 @@
 # mortgage.py
 #
 # Exercise 1.7
+pricipal = 500000.0
+rate = 0.05
+payment = 2684.11
+total = 0
+mouth = 0
+start = 60
+end = 108
+while pricipal > 0:
+    mouth += 1
+    if mouth >= start and mouth <= end:
+        pricipal = pricipal * (1+rate/12) - payment - 1000
+        total += payment + 1000
+    else:
+        pricipal = pricipal * (1+rate/12) - payment
+        total += payment
+
+    print(round(total, 2), mouth)
